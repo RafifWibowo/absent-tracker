138e8de3d030132b9f0d5778e89d7e92a91cda47
ramammurshal <rmurshal@gmail.com>
2022-12-13 02:44:52 +0700

    feat: register event, hide when already register

--- app/src/main/java/com/example/fpmobile/event_details.java
+++ app/src/main/java/com/example/fpmobile/event_details.java
@@ -3,17 +3,27 @@ package com.example.fpmobile;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class event_details extends AppCompatActivity {
     private ImageView img;
@@ -21,6 +31,7 @@ public class event_details extends AppCompatActivity {
     private TextView desc;
     private TextView place;
     private TextView time;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,9 @@ public class event_details extends AppCompatActivity {
         time = findViewById(R.id.detail_time);
 
         String id = getIntent().getStringExtra("id");
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String userUid = currentUser.getUid();
 
         DocumentReference docRef = FirebaseFirestore.getInstance().collection("events").document(id);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -55,5 +69,55 @@ public class event_details extends AppCompatActivity {
                 }
             }
         });
+
+        FirebaseFirestore
+                .getInstance()
+                .collection("absen")
+                .whereEqualTo("event_id", id)
+                .whereEqualTo("user_id", userUid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            boolean isUserRegistered = false;
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                if (doc.exists()) {
+                                    isUserRegistered = true;
+                                }
+                            }
+
+                            btn = findViewById(R.id.detail_btn);
+                            if (isUserRegistered) {
+                                   btn.setVisibility(View.GONE);
+                            } else {
+                                btn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                        Map<String, Object> data = new HashMap<>();
+                                        data.put("event_id", id);
+                                        data.put("user_id", userUid);
+                                        data.put("status", false);
+                                        db.collection("absen").add(data).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentReference> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(event_details.this, "Sukses daftar event", Toast.LENGTH_SHORT).show();
+                                                    startActivity(new Intent(event_details.this, ProfileActivity.class));
+                                                    finish();
+                                                } else {
+                                                    Toast.makeText(event_details.this, "Gagal daftar event", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        } else {
+                            Toast.makeText(event_details.this, "Gagal mendapatkan data absen", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
